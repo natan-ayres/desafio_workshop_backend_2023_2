5aa0cedaaad227e920feb2e38f4f4e032822b52d
Natan Ayres <129100066+natan-ayres@users.noreply.github.com>
2023-09-02 13:00:56 -0300

    Update Settings.py

--- Projeto/settings.py
+++ Projeto/settings.py
@@ -78,11 +78,8 @@ WSGI_APPLICATION = 'Projeto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'FilmesPostgresDB',
-        'USER':'postgres',
-        'PASSWORD':'django123',
-        'HOST':'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
